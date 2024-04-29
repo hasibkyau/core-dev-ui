8e26c38f0beb40217a1f7f2bfb42f4bae20c7850
unknown <hasibkyau.cse@gmail.com>
2024-04-29 08:05:51 +0600

    resolved issue with import Checkout page

--- src/Pages/CheckoutPage.jsx
+++ src/Pages/CheckoutPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { useGlobalCtx } from '../Contexts/GlobalProvider';
-import Checkout from '../Features/Checkout/Components/Checkout';
+import {Checkout} from '../Features/Checkout/Components/Checkout';
 
 export default function CheckoutPage() {
     const [searchParams] = useSearchParams();
